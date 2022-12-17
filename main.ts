6c41893a57618efc2089c9c5f1b9c11d52e229a9
Andi <andi@go-for-fun.de>
2022-12-17 14:28:05 +0100

    v1

--- main.ts
+++ main.ts
@@ -7,215 +7,220 @@
  */
 //% weight=100 color=#50A820 icon="8"
 namespace TM1638 {
-    let TM1638_CMD1 = 0x40;
-    let TM1638_CMD2 = 0xC0;
-    let TM1638_CMD3 = 0x80;
-    let _SEGMENTS = [0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71];
+
+    const fontMap = [
+        0x00, /* (space) */
+        0x86, /* ! */
+        0x22, /* " */
+        0x7E, /* # */
+        0x6D, /* $ */
+        0xD2, /* % */
+        0x46, /* & */
+        0x20, /* ' */
+        0x29, /* ( */
+        0x0B, /* ) */
+        0x21, /* * */
+        0x70, /* + */
+        0x10, /* , */
+        0x40, /* - */
+        0x80, /* . */
+        0x52, /* / */
+        0x3F, /* 0 */
+        0x06, /* 1 */
+        0x5B, /* 2 */
+        0x4F, /* 3 */
+        0x66, /* 4 */
+        0x6D, /* 5 */
+        0x7D, /* 6 */
+        0x07, /* 7 */
+        0x7F, /* 8 */
+        0x6F, /* 9 */
+        0x09, /* : */
+        0x0D, /* ; */
+        0x61, /* < */
+        0x48, /* = */
+        0x43, /* > */
+        0xD3, /* ? */
+        0x5F, /* @ */
+        0x77, /* A */
+        0x7C, /* B */
+        0x39, /* C */
+        0x5E, /* D */
+        0x79, /* E */
+        0x71, /* F */
+        0x3D, /* G */
+        0x76, /* H */
+        0x30, /* I */
+        0x1E, /* J */
+        0x75, /* K */
+        0x38, /* L */
+        0x15, /* M */
+        0x37, /* N */
+        0x3F, /* O */
+        0x73, /* P */
+        0x6B, /* Q */
+        0x33, /* R */
+        0x6D, /* S */
+        0x78, /* T */
+        0x3E, /* U */
+        0x3E, /* V */
+        0x2A, /* W */
+        0x76, /* X */
+        0x6E, /* Y */
+        0x5B, /* Z */
+        0x39, /* [ */
+        0x64, /* \ */
+        0x0F, /* ] */
+        0x23, /* ^ */
+        0x08, /* _ */
+        0x02, /* ` */
+        0x5F, /* a */
+        0x7C, /* b */
+        0x58, /* c */
+        0x5E, /* d */
+        0x7B, /* e */
+        0x71, /* f */
+        0x6F, /* g */
+        0x74, /* h */
+        0x10, /* i */
+        0x0C, /* j */
+        0x75, /* k */
+        0x30, /* l */
+        0x14, /* m */
+        0x54, /* n */
+        0x5C, /* o */
+        0x73, /* p */
+        0x67, /* q */
+        0x50, /* r */
+        0x6D, /* s */
+        0x78, /* t */
+        0x1C, /* u */
+        0x1C, /* v */
+        0x14, /* w */
+        0x76, /* x */
+        0x6E, /* y */
+        0x5B, /* z */
+    ]
 
     /**
      * TM1638 LED display
      */
     export class TM1638LEDs {
-        buf: Buffer;
         clk: DigitalPin;
         dio: DigitalPin;
         strobe: DigitalPin;
-        _ON: number;
         brightness: number;
         count: number;  // number of LEDs
 
         /**
          * initial TM1638
          */
-        init(): void {
-            pins.digitalWritePin(this.clk, 0);
-            pins.digitalWritePin(this.dio, 0);
-            pins.digitalWritePin(this.strobe, 1);
-            this._ON = 8;
-            this.buf = pins.createBuffer(this.count);
-            this._write_data_cmd(0x40);
-            this._write_data_cmd(0xC0);
+        setup(): void {
+            pins.digitalWritePin(this.dio, 0)
+            pins.digitalWritePin(this.clk, 1)
+            pins.digitalWritePin(this.strobe, 1)
+            this.sendCommand(143);
+            this.setBrightness(7);
             this.clear();
         }
 
-        /**
-         * Start 
-         */
-        _start() {
-            pins.digitalWritePin(this.dio, 0);
-            pins.digitalWritePin(this.clk, 0);
+        startCommand() {
             pins.digitalWritePin(this.strobe, 0);
         }
 
-        /**
-         * Stop
-         */
-        _stop() {
-            pins.digitalWritePin(this.dio, 0);
-            pins.digitalWritePin(this.clk, 1);
-            pins.digitalWritePin(this.dio, 1);
-            pins.digitalWritePin(this.strobe, 1);
+        sendCommand(num: number) {
+            this.startCommand();
+            this.writeByte(num);
+            this.endCommand();
         }
 
-        /**
-         * send command1
-         */
-        _write_data_cmd(cmd: number) {
-            this._start();
-            this._write_byte(cmd);
-            this._stop();
-        }
-
-        /**
-         * send command3
-         */
-        _write_dsp_ctrl() {
-            this._start();
-            this._write_byte(TM1638_CMD3 | this._ON | this.brightness);
-            this._stop();
-        }
-
-        /**
-         * send a byte to 2-wire interface
-         */
-        _write_byte(b: number) {
-            for (let i = 0; i < 8; i++) {
-                pins.digitalWritePin(this.dio, (b >> i) & 1);
-                pins.digitalWritePin(this.clk, 1);
-                pins.digitalWritePin(this.clk, 0);
+        writeByte(num: number) {
+            for (let j = 0; j <= 7; j++) {
+                pins.digitalWritePin(this.dio, (num >> j) & 1)
+                pins.digitalWritePin(this.clk, 1)
+                pins.digitalWritePin(this.clk, 0)
             }
-            pins.digitalWritePin(this.clk, 1);
-            pins.digitalWritePin(this.clk, 0);
+        }
+
+        endCommand() {
+            pins.digitalWritePin(this.strobe, 1)
         }
 
         /**
          * set TM1638 intensity, range is [0-8], 0 is off.
-         * @param val the brightness of the TM1638, eg: 7
+         * @param brightness the brightness of the TM1638, eg: 7
          */
-        //% blockId="TM1638_set_intensity" block="%tm|set intensity %val"
+        //% blockId="TM1638_set_intensity" block="%tm|set intensity %brightness"
         //% weight=50 blockGap=8
         //% parts="TM1638"
-        intensity(val: number = 7) {
-            if (val < 1) {
-                this.off();
-                return;
-            }
-            if (val > 8) val = 8;
-            this._ON = 8;
-            this.brightness = val - 1;
-            this._write_data_cmd(128 + this.brightness);
+        setBrightness(brightness: number = 7) {
+            let value = 136 + (0x07 & brightness)
+            this.sendCommand(value)
         }
 
         /**
-         * set data to TM1638, with given bit
+         * show a number.
+         * @param num is a number, eg: 0
          */
-        _dat(bit: number, dat: number) {
-            this._start();
-            this._write_byte(dat)
-            this._stop();
-        }
-
-        /**
-         * show a number in given position. 
-         * @param num number will show, eg: 5
-         * @param bit the position of the LED, eg: 0
-         */
-        //% blockId="TM1638_showbit" block="%tm|show digit %num |at %bit"
-        //% weight=90 blockGap=8
-        //% parts="TM1638"
-        showbit(num: number = 5, bit: number = 0) {
-            this.buf[bit % this.count] = _SEGMENTS[num % 16]
-            this._dat(bit, _SEGMENTS[num % 16])
-        }
-
-        /**
-          * show a number. 
-          * @param num is a number, eg: 0
-          */
         //% blockId="TM1638_shownum" block="%tm|show number %num"
         //% weight=91 blockGap=8
         //% parts="TM1638"
-        showNumber(num: number) {
-            if (num < 0) {
-                this._dat(0, 0x40) // '-'
-                num = -num
+        showNumber (num: number) {
+            let strg = "" + num
+            let vals = strg.split('')
+            let offset = 8 - vals.length
+            for (let i = offset; i <= 7; i++) {
+                this.show7Segment(i, fontMap[16 + parseInt(vals[i-offset])])
             }
-            else
-                this.showbit(Math.idiv(num, 1000) % 10)
-            this.showbit(num % 10, 3)
-            this.showbit(Math.idiv(num, 10) % 10, 2)
-            this.showbit(Math.idiv(num, 100) % 10, 1)
         }
 
         /**
-          * show a hex number. 
-          * @param num is a hex number, eg: 0
-          */
-        //% blockId="TM1638_showhex" block="%tm|show hex number %num"
-        //% weight=90 blockGap=8
-        //% parts="TM1638"
-        showHex(num: number) {
-            if (num < 0) {
-                this._dat(0, 0x40) // '-'
-                num = -num
-            }
-            else
-                this.showbit((num >> 12) % 16)
-            this.showbit(num % 16, 3)
-            this.showbit((num >> 4) % 16, 2)
-            this.showbit((num >> 8) % 16, 1)
-        }
-
-        /**
-         * show or hide dot point. 
-         * @param bit is the position, eg: 1
-         * @param show is show/hide dp, eg: true
+         * shows text output on 7 segment displa<y
+         * @param text text output to display
          */
-        //% blockId="TM1638_showDP" block="%tm|DotPoint at %bit|show %show"
+        //% blockId="TM1638_showText" block="%tm|display text %text"
         //% weight=70 blockGap=8
         //% parts="TM1638"
-        showDP(bit: number = 1, show: boolean = true) {
-            bit = bit % this.count
-            if (show) this._dat(bit, this.buf[bit] | 0x80)
-            else this._dat(bit, this.buf[bit] & 0x7F)
+        showText (text: string) {
+            let vals = text.toUpperCase().split('')
+            for (let i = 0; i < Math.min(vals.length, 8); i++) {
+                console.log(vals[i].charCodeAt(0));
+                this.show7Segment(i, fontMap[vals[i].charCodeAt(0) - 32])
+            }
         }
 
         /**
-         * clear LED. 
+         * show font based output on 7 segment display
+         * @param position display number
+         * @param value byte value to show
+         */
+        //% blockId="TM1638_show7seg" block="%tm|show 7 segment at %position|show %value"
+        //% weight=70 blockGap=8
+        //% parts="TM1638"
+        show7Segment(position: number, value: number) {
+            this.sendCommand(0x44)
+            this.startCommand()
+            this.writeByte(0xC0 + (position << 1))
+            this.writeByte(value)
+            this.endCommand()
+        }
+
+        /**
+         * clear LED.
          */
         //% blockId="TM1638_clear" block="clear %tm"
         //% weight=80 blockGap=8
         //% parts="TM1638"
         clear() {
-            for (let i = 0; i < this.count; i++) {
-                this._dat(i, 0)
-                this.buf[i] = 0
+            this.sendCommand(64);
+            this.startCommand();
+            this.writeByte(192);
+            for (let index = 0; index < 16; index++) {
+                this.writeByte(0);
             }
-        }
-
-        /**
-         * turn on LED. 
-         */
-        //% blockId="TM1638_on" block="turn on %tm"
-        //% weight=86 blockGap=8
-        //% parts="TM1638"
-        on() {
-            this._ON = 8;
-            this._write_data_cmd(134);
-        }
-
-        /**
-         * turn off LED. 
-         */
-        //% blockId="TM1638_off" block="turn off %tm"
-        //% weight=85 blockGap=8
-        //% parts="TM1638"
-        off() {
-            this._ON = 0;
-            this._write_data_cmd(128);
+            this.endCommand();
         }
     }
-
     /**
      * create a TM1638 object.
      * @param clk the CLK pin for TM1638, eg: DigitalPin.P1
@@ -234,7 +239,7 @@ namespace TM1638 {
         if ((count < 1) || (count > 9)) count = 8;
         tm.count = count;
         tm.brightness = intensity;
-        tm.init();
+        tm.setup();
         return tm;
     }
 }
